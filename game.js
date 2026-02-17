ba311f36fb4ab71d9e2385c6dd4a583d9a6afd24
Ivan KOCHERGIN <Ivan.KOCHERGIN@nkteh-group.ru>
2026-02-17 17:15:40 +0300

    init

--- game.js
+++ game.js
@@ -1,1 +1,48 @@
-console.log('111');
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
+let col = 0;
+let row = 0;
+
+function getGridDiv(col, row) {
+  const grid = document.querySelector(`.game-map-grid`).querySelectorAll('.game-map');
+  return grid[col].querySelectorAll('.box')[row];
+}
+
+function addDivKey(key) {
+  console.log(col, row);
+  const selectedDiv = getGridDiv(col, row);
+  if (col > 3) {
+    selectedDiv.innerText = key.toUpperCase();
+  } else {
+    selectedDiv.innerText = key.toUpperCase();
+    col++;
+  }
+}
+
+function handleEnter() {
+  if (col < 3) {
+    console.log('ничего');
+  }
+}
+
+function initGame() {
+  document.addEventListener('keydown', function (event) {
+    if (!isLetter(event.key)) {
+      switch (event.key) {
+        case 'Enter':
+          console.log(getGridDiv(col, row).innerText.length > 0);
+          if (getGridDiv(col, row).innerText.length > 0) {
+            row++;
+            col = 0;
+          }
+          break;
+      }
+    } else {
+      addDivKey(event.key);
+    }
+  });
+}
+
+initGame();
