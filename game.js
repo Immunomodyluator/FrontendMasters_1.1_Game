5bc4206483d0b8d5e1f823af5a558a168e6f6c6c
Ivan Kochergin <venhesterhack@gmail.com>
2026-02-18 16:11:09 +0300

    Add word validation and styling to the word game

--- game.js
+++ game.js
@@ -2,6 +2,21 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
+async function getWord() {
+  const promise = await fetch('https://words.dev-apis.com/word-of-the-day');
+  const proceedResponse = await promise.json();
+  return proceedResponse.word;
+}
+
+async function sendWord() {
+  const sendWord = await fetch('https://words.dev-apis.com/validate-word', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ resultWord }),
+  });
+  return await sendWord.json();
+}
+
 let col = 0;
 let row = 0;
 
@@ -11,19 +26,44 @@ function getGridDiv(col, row) {
 }
 
 function addDivKey(key) {
-  console.log(col, row);
-  const selectedDiv = getGridDiv(col, row);
-  if (col > 3) {
-    selectedDiv.innerText = key.toUpperCase();
-  } else {
-    selectedDiv.innerText = key.toUpperCase();
+  getGridDiv(col, row).innerText = key.toUpperCase();
+  if (col < 4) {
     col++;
   }
 }
 
 function handleEnter() {
-  if (col < 3) {
-    console.log('ничего');
+  let enteredWord = '';
+  for (let i = 0; i < 5; i++) {
+    enteredWord += getGridDiv(i, row).innerText;
+  }
+  getWord().then((word) => {
+    word = word.toUpperCase();
+    if (word === enteredWord) {
+      console.log('выйграл');
+    } else {
+      console.log('проиграл');
+    }
+
+    for (let i = 0; i < 5; i++) {
+      if (enteredWord[i] === word[i]) {
+        getGridDiv(i, row).classList.add('the-required-letter');
+      }
+    }
+    if (getGridDiv(4, row).innerText !== '') {
+      row++;
+      col = 0;
+    }
+  });
+}
+
+function removeKeyFromDiv() {
+  const div = getGridDiv(col, row);
+  if (col > 0 && div.innerText === '') {
+    col--;
+    getGridDiv(col, row).innerText = '';
+  } else if (div.innerText !== '') {
+    getGridDiv(col, row).innerText = '';
   }
 }
 
@@ -32,11 +72,10 @@ function initGame() {
     if (!isLetter(event.key)) {
       switch (event.key) {
         case 'Enter':
-          console.log(getGridDiv(col, row).innerText.length > 0);
-          if (getGridDiv(col, row).innerText.length > 0) {
-            row++;
-            col = 0;
-          }
+          handleEnter();
+          break;
+        case 'Backspace':
+          removeKeyFromDiv();
           break;
       }
     } else {
